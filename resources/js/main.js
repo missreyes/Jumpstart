707570a7ef88788d97c9c6a6d86ae95803f3e24c
missreyes <username@users.noreply.github.com>
2017-09-05 00:51:02 -0700

    Added Slick, main.js, fixed btns, removed borders.

---
+++ resources/js/main.js
@@ -0,0 +1,32 @@
+// Main js by Shari Reyes
+$(document).ready(function(){
+
+  $('.carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  });
+
+var 'userScrolled' = 0;
+$(window).scroll (function()) {
+   var 'scrollUp' = $(this).scrollTop();
+   if ('scrollUp' - 'userScrolled' < 50) {
+       var 'newHeight' = $('.navbar').css('height');
+       $('.navbar').animate({top: '-' + 'newHeight'}, 150);
+       'userScrolled' = 'scrollUp'
+     } else {
+       ('scrollUp' - 'userScrolled' > 50) {
+           $('.navbar').animate({top: '0px'}, 150);
+           'userScrolled' = 'scrollUp'
+
+     }
+
+   }
+}
+
+});
